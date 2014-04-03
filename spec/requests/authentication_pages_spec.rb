d55b725b8e442a41f6b33d539c24d9b4e28af5a9
Juan C. Nieves <juanc.nieves@gmail.com>
2014-04-03 12:06:59 -0400

    Add aditional test to remove the Sign up link when the user is signed in

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -15,7 +15,7 @@ describe 'Authentication' do
     describe 'With invalid information' do
       before { click_button 'Sign in' }
       it { should have_title('Sign in') }
-      it { should have_flash_message('danger', 'Invalid')  }
+      it { should have_flash_message('danger', 'Invalid') }
       describe 'After visiting another page' do
         before { click_link 'Home' }
         it { should_not have_selector(flash_alert('danger')) }
@@ -34,6 +34,11 @@ describe 'Authentication' do
       it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
+
+      describe 'After visiting Home page' do
+        before { click_link 'Home' }
+        it { should_not have_link('Sign up now!', href: signup_path) }
+      end
 
       describe 'Followed by signout' do
         before { click_link 'Sign out' }
